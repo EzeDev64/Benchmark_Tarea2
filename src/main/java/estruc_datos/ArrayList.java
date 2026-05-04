83a433255ab76e5b27dc6c026ac3266eb9bb5995
Ezequiel <ezequielbv2305@gmail.com>
2026-05-04 15:29:16 -0600

    feat: ArrayList y LinkedList.

---
+++ src/main/java/estruc_datos/ArrayList.java
@@ -0,0 +1,102 @@
+package estruc_datos;
+
+public class ArrayList <T>{
+    private Object[] list;
+    private int size;
+
+    public ArrayList(){
+        this.size = 0;
+        this.list = null;
+    }
+
+    @SuppressWarnings("unchecked")
+    public void push(T data){
+        if (this.list == null){
+            this.list = new Object[1];
+            this.list[0] = data;
+            this.size += 1;
+        }else{
+            Object[] temp = new Object[this.size+1];
+            for(int i=0;i<this.list.length;i++){
+                temp[i] = this.list[i];
+            }
+            temp[this.size] = data;
+            this.list = temp;
+            this.size += 1;
+        }
+    }
+
+    public void insert(Object data,int position){
+        if (position > size-1 && position !=0){
+            throw new IndexOutOfBoundsException("Index está fuera de los indices de la lista");
+        }
+
+        if (this.list == null){
+            this.list = new Object[1];
+            this.list[0] = data;
+            this.size += 1;
+        }else{
+            if (position == 0){
+                Object[] temp = new Object[this.size+1];
+                temp[0]  = data;
+                for(int i=0;i<this.list.length;i++){
+                    temp[i+1] = this.list[i];
+                }
+                this.list = temp;
+                this.size += 1;
+            }else if(position == this.size-1){
+                Object[] temp = new Object[this.size+1];
+                for(int i=0;i<this.list.length;i++){
+                    temp[i] = this.list[i];
+                }
+                temp[this.size] = data;
+                this.list = temp;
+                this.size += 1;
+            }else{
+                Object[] temp = new Object[this.size+1];
+                for(int i=0;i<position;i++){
+                    temp[i] = this.list[i];
+                }
+                temp[position] = data;
+                for(int i=position;i<this.size;i++){
+                    temp[i+1] = this.list[i];
+                }
+                this.list = temp;
+                this.size += 1;
+            }
+        }
+        
+    }
+
+    public void delete(int position){
+        if (position > size-1 && position !=0){
+            throw new IndexOutOfBoundsException("Index está fuera de los indices de la lista");
+        }
+        if (this.list == null){
+            throw new NullPointerException("No hay ningún elemento dentro de la lista");
+        }
+
+        Object[] temp = new Object[this.size-1];
+        for(int i=0;i<position;i++){
+                    temp[i] = this.list[i];
+        }
+        for(int i=position;i<this.size-1;i++){
+            temp[i] = this.list[i+1];
+        }
+                
+        this.list = temp;
+        this.size -= 1;
+    }
+
+    public Object getAt(int position){
+        if (position > this.size-1 && position !=0){
+            throw new IndexOutOfBoundsException("Index está fuera de los indices de la lista");
+        }
+        return this.list[position];
+    }
+
+    public int getSize(){
+        return this.size;
+    }
+
+}
---
+++ src/main/java/estruc_datos/LinkedList.java
@@ -0,0 +1,147 @@
+package estruc_datos;
+
+public class LinkedList <T extends Comparable<? super T>> {
+    private Node<T> head;
+    private Node<T> tail;
+    private int size;
+
+    public LinkedList(){
+        head = null;
+        tail = null;
+        size = 0;
+    }
+
+    public void push(T data){
+        Node<T> node = new Node<T>(data);
+        
+        if (size==0){
+            this.head = node;
+            this.tail = node;
+            this.size += 1;
+        }else{
+            this.tail.setNext(node);
+            this.tail = node;
+            this.size += 1;
+        }
+
+
+    }
+
+    public void insert(T data, int position){
+        if (position > size-1 && position !=0){
+            throw new IndexOutOfBoundsException("Index está fuera de los indices de la lista");
+        }
+
+        Node<T> node = new Node<T>(data);
+        
+        //Si la lista está vacía
+        if (size==0){
+            this.head = node;
+            this.tail = node;
+            this.size += 1;
+        }else{ 
+            if (position == 0){
+                Node<T> first = this.head;
+                node.setNext(first);
+                this.head = node;
+                this.size += 1;
+            }else if (position == this.size-1){
+                this.tail.setNext(node);
+                this.tail = node;
+                this.size += 1;
+            }else{
+                Node<T> prev = this.head;
+                for (int i=0;i<this.size;i++){
+                    if(i == position-1){
+                        break;
+                    }else{
+                        prev = prev.getnext();
+                    }
+                }
+
+                node.setNext(prev.getnext());
+                prev.setNext(node);
+                this.size += 1;
+            }
+        }
+    }
+
+    public void delete(int position){
+        if (position > size-1 && position !=0){
+            throw new IndexOutOfBoundsException("Index está fuera de los indices de la lista");
+        }
+        if (this.head == null){
+            throw new NullPointerException("No hay ningún elemento dentro de la lista");
+        }
+
+        if (position == 0){
+            this.head = head.getnext();
+        }else{
+            Node<T> prev = this.head;
+            for (int i=0;i<this.size;i++){
+                if(i == position-1){
+                    break;
+                }else{
+                    prev = prev.getnext();
+                }
+            }
+            prev.setNext(prev.getnext().getnext());
+            if(position==this.size-1){
+                this.tail = prev;
+            }
+        }
+
+        this.size-=1;
+    }
+
+    public T getAt(int position){
+        if (position > size-1 && position !=0){
+            throw new IndexOutOfBoundsException("Index está fuera de los indices de la lista");
+        }
+
+        Node<T> node = this.head;
+
+        for (int i=0;i<this.size;i++){
+            if(i == position){
+                return node.getData();
+            }else{
+                node = node.getnext();
+            }
+        }
+
+        return null;
+    }
+    public void HeadTail(){
+        System.out.print(this.head.getData());
+        System.out.print(" ");
+        System.out.println(this.tail.getData());
+    }
+
+    public int getSize(){
+        return this.size;
+    }
+
+}
+
+class Node <T extends Comparable<? super T>>{
+        private Node<T> next;
+        private T data;
+
+        public Node(T data){
+            this.data = data;
+            this.next = null;
+        }
+
+        public Node<T> getnext(){
+            return this.next;
+        }
+
+        public void setNext(Node<T> node){
+            this.next = node;
+        }
+
+        public T getData(){
+            return this.data;
+        }
+
+}
